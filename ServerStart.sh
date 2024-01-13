f80b618632dfe8a5958242954f265126779187c7
Salman8932 <keepitsimpleandgreat@gmail.com>
2024-01-14 01:17:06 +0800

    14/1/2024 1:16am

--- ServerStart.sh
+++ ServerStart.sh
@@ -5,7 +5,7 @@
 
 # Start the server.
 start_server() {
-"/usr/java/jre1.8.0_391/bin/java" -server -Xms3G -Xmx5G ${JAVA_PARAMETERS} -jar ${SERVER_JAR} nogui
+"/usr/java/jre1.8.0_391/bin/java" -server -Xms4G -Xmx6G ${JAVA_PARAMETERS} -jar ${SERVER_JAR} nogui
 }
 
 echo "Starting Sky Factory 4 Server..."
